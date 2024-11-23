125d7b832515d191dd51d157e6344dd29ddf79b2
Labib <labib@0xfaisal.local>
2024-11-23 15:58:47 +0600

    Updated

--- app.py
+++ app.py
@@ -12,5 +12,21 @@ def login():
         return render_template('login.html')
     return '<h1> Trying To Login.<br> You naughty </h1>'
 
+@app.route("/signup", methods = ['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    return '<h1> Trying To Signup.<br> You naughty </h1>'
+
+@app.route("/forgot", methods = ['GET', 'POST'])
+def forgot():
+    if request.method == 'GET':
+        return render_template('forgot.html')
+    return '<h1> Trying To reset password.<br> You naughty </h1>'
+
+@app.route("/logout", methods = ['GET'])
+def logout():
+    return '<h1> Logout </h1>'
+
 if __name__ == '__main__':
     app.run(host = "0.0.0.0", debug = True, port = 8080)
