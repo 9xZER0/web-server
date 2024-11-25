a131ae3eced0a53fbcbe6ff042cd2e976a13da03
Labib <labib@0xfaisal.local>
2024-11-25 23:40:44 +0600

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect # type: ignore
+from flask import Flask, render_template, flash, redirect, request # type: ignore
 import requests # type: ignore
 
 app = Flask(__name__)
@@ -8,18 +8,36 @@ API_URL = "http://127.0.0.1:8000"
 # Home page
 @app.route("/")
 def home():
-    response = requests.get(f"{API_URL}/")
-    response.raise_for_status()
-    message = response.json()
-    return f"HELLO <br> {message['messages']} <br>"
+    try:
+        response = requests.get(f"{API_URL}/")
+        response.raise_for_status()
+        message = response.json()
+        return f"HELLO <br> {message['messages']} <br>"
+    except requests.exceptions.Timeout:
+        return "Can not connect to api"
 
 # Login page
-# @app.route("/login", methods = ['GET', 'POST'])
-# def login():
-#     if request.method == 'GET':
-#         return render_template('login.html')
-#     _email = request.form['email']
-#     _passWord = request.form['password']
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    try:
+        _email = request.form['email']
+        _passWord = request.form['password']
+        payload = {
+            "email" : _email,
+            "password" : _passWord
+        }
+        response = requests.post(f"{API_URL}/verify_user", json = payload)
+        response.raise_for_status()
+        message = response.json()
+        if message['messages'] == "verified":
+            return "Login SuccessFul"
+        elif message['messages'] == "not found":
+            return "No user found"
+        return "Invalid Credentcial"
+    except requests.exceptions.Timeout:
+        return "Can not connect to api"
 
 # Signup page
 # @app.route("/signup", methods = ['GET', 'POST'])
