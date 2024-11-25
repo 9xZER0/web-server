4f77197cdddb241c221923256f8dbd8be11e93d8
Labib <labib@0xfaisal.local>
2024-11-25 16:01:28 +0600

    update

--- app.py
+++ app.py
@@ -1,43 +1,36 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
 
 app = Flask(__name__)
+app.secret_key = "noSystemIsSafe"
+API_URL = "http://api"
 
 # Home page
 @app.route("/")
 def home():
-    return "Do You need Authentication ?"
 
 # Login page
 @app.route("/login", methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('login.html')
-    
     _email = request.form['email']
     _passWord = request.form['password']
-    
-    return f'<h1> Trying To Login {_email} . {_passWord} </h1>'
 
 # Signup page
 @app.route("/signup", methods = ['GET', 'POST'])
 def signup():
     if request.method == 'GET':
         return render_template('signup.html')
-    
     _email = request.form['email']
     _userName = request.form['username']
     _passWord = request.form['password']
-
-    return '<h1> Trying To Signup.<br> You naughty </h1>'
 
 # Forgot password page
 @app.route("/forgot", methods = ['GET', 'POST'])
 def forgot():
     if request.method == 'GET':
         return render_template('forgot.html')
-    
     _email = request.form['email']
-    return '<h1> Trying To reset password.<br> You naughty </h1>'
 
 # Logout page
 @app.route("/logout", methods = ['GET'])
